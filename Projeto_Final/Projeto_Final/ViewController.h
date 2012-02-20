71d963bb6277d30fa96b747f888154ffce6743dc
Felipe Alves <felipe@bitix.com.br>
2012-02-20 21:10:55 -0200

    Criacao do projeto

---
+++ Projeto_Final/Projeto_Final/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Projeto_Final
+//
+//  Created by Felipe Alves on 20/02/12.
+//  Copyright (c) 2012 Bitix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Projeto_Final/Projeto_Final/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Projeto_Final
+//
+//  Created by Felipe Alves on 20/02/12.
+//  Copyright (c) 2012 Bitix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
